2105356f8386e3fb3c934edd56e6b8e23403df9e
KubaKraken <k.jakub.dev.007@gmail.com>
2021-05-06 21:08:10 +0200

    Initial commit

---
+++ src/main/java/com/kuba/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.kuba.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+// enable zuul  proxy
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+
+
+    }
+
+
+
+
+}
